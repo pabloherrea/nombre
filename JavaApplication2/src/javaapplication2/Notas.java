fa95e86c60c11e46564e31270e31d757c5b77041
Jose Herrera <jpfloresh@alumno.uspg.edu.gt>
2020-10-18 03:09:51 -0600

    mi nombre

---
+++ JavaApplication2/src/javaapplication2/Notas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Jose Herrera
+ */
+public class Notas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Hola mi nombre es pablo
+        // TODO code application logic here
+    }
+    
+}
